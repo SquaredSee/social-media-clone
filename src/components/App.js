13a802be3a5c154e94475c5ecc683bca14f2304f
Colton Crouch <colton.crouch96@gmail.com>
2017-06-28 14:26:04 -0400

    edit placeholder routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
 
-
 const App = () => (
   <div id="app" style={{ borderStyle: 'solid' }}>
     <p>This is the App component</p>
@@ -10,8 +9,8 @@ const App = () => (
     <div id="pagewrapper">
       <Switch>
         <Route exact path="/" render={() => <div>home</div>} />
-        <Route exact path="/post" render={() => <div>post id not found</div>} />
         <Route path="/post/:id" render={({ match }) => <div>{`Post ID: ${match.params.id}`}</div>} />
+        <Route exact path="*" render={() => <div>404</div>} />
       </Switch>
     </div>
   </div>
