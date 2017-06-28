834d50ea396ef752c89ca9d3d5b8a25ada7565c7
Colton Crouch <colton.crouch96@gmail.com>
2017-06-28 11:42:03 -0400

    add support for gh-pages and add basic app layout

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import { Switch, Route, Link } from 'react-router-dom';
+
+
+const App = () => (
+  <div style={{ borderStyle: 'solid' }}>This is the App component</div>
+);
+
+export default App;
