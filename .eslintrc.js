51f65e430342e243ba6adf2b68cd83ce1ba5f542
Colton Crouch <colton.crouch96@gmail.com>
2017-06-28 11:31:53 -0400

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "airbnb",
+  "env": {
+    "browser": true,
+  },
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "rules": {
+    "brace-style": [1, "stroustrup"],
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  }
+};
