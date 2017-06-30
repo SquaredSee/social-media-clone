3eda274176b4396c99e03b9c34051541577e67ad
Colton Crouch <colton.crouch96@gmail.com>
2017-06-30 12:06:17 -0400

    add new scripts to package.json for better workflow

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 7000;
+app.use(express.static(path.join(__dirname, 'build')));
 
-const router = express.Router();
-router.get('/', (req, res) => {
+// const router = express.Router();
+app.get('/api', (req, res) => {
   res.json({ message: 'api root' });
 });
 
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+// app.use('/api', router);
 
-app.use(express.static('build'));
+const port = process.env.PORT || 7000;
 
-// app.get('/', (req, res) => {
-//   const index = path.join(__dirname, 'build/index.html');
-//   console.log(index);
-//   res.sendFile(index);
-// });
+// serve build only if run with NODE_ENV=production
+// run client and server seperately during development
+if (process.env.NODE_ENV === 'production') {
+  console.log(`Serving client at http://localhost:${port}`);
+  app.get('*', (req, res) => {
+    const index = path.join(__dirname, 'build', 'index.html');
+    console.log(index);
+    res.sendFile(index);
+  });
+}
 
 app.listen(port, () => {
-  console.log(`Api started on port ${port}`);
+  console.log(`Api started at http://locahost:${port}/api`);
 });
