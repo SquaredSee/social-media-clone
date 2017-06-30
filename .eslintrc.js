a04dc8f81d4b59f51006170439b722553756f0bc
Colton Crouch <colton.crouch96@gmail.com>
2017-06-30 16:14:19 -0400

    turned off class-methods-use-this

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   "rules": {
     "arrow-body-style": [0],
     "brace-style": [1, "stroustrup"],
+    "class-methods-use-this": [0],
     "no-console": [0],
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
   }
