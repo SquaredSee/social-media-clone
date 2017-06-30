25002958dea3f42f5983d018f4b25c8a9c9ea6c3
Colton Crouch <colton.crouch96@gmail.com>
2017-06-30 15:43:22 -0400

    turn off arrow-body-style

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "import"
   ],
   "rules": {
+    "arrow-body-style": [0],
     "brace-style": [1, "stroustrup"],
     "no-console": [0],
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
